6fff533786d172201ce0c690aeaf13b02aca1071
Alberto Zordan <zorberto@gmail.com>
2020-09-17 15:23:54 +0200

    Some tweaking...

--- get_access.php
+++ get_access.php
@@ -1,6 +1,5 @@
-<!-- Passare come parametri GET name, mail ed age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
-
 <?php
+/* Passare come parametri GET name, mail ed age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
 $name = $_GET['name'];
 $mail = $_GET['mail'];
 $age = $_GET['age'];
@@ -8,11 +7,11 @@ $age = $_GET['age'];
 if (empty($name && $mail && $age)) {
     $message = 'Non hai inserito tutti i dati';
 } else {
-    $checkAll = strlen($name) > 3 && strpos($mail,'@') > -1 && strpos($mail,'.') > -1 && ctype_digit($age);
+    $checkAll = strlen($name) > 3 && strpos($mail, '@') > -1 && strpos($mail, '.') > -1 && ctype_digit($age);
     $checkAll ? $message = 'Accesso riuscito' : $message = 'Accesso negato';
 }
 echo $message;
-?>
 
-<!-- Confronta: intval, is_numeric, is_int, (int)$var.
-Nota: is_nan verifica solo che l'espressione non sia un particolare valore di tipo float denominato NaN, il quale non può essere rappresentato/interpretato come numero reale. Non fornisce info sul tipo di dato. Ecco perché restituisce false con una stringa o anche con true/false booleani -->
+// Confronta: intval, is_numeric, is_int, (int)$var.
+// Nota: is_nan verifica solo che l'espressione non sia un particolare valore di tipo float denominato NaN, il quale non può essere rappresentato/interpretato come numero reale. Non fornisce info sul tipo di dato. Ecco perché restituisce false con una stringa o anche con true/false booleani
+?>
